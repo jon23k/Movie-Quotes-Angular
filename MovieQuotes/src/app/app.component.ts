6e940238ce1ec788083196ccd7f0555a81a59cff
Jonathan  Kurian <kurianj@rose-hulman.edu>
2017-06-13 18:40:33 -0700

    local version done!

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -12,8 +12,25 @@ interface MovieQuote{
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  movieQuote: MovieQuote ={
+  formMovieQuote: MovieQuote ={
     'quote': '',
     'movie': '',
   };
+
+  movieQuotes: MovieQuote[] = [
+    {"movie": "Rocky", "quote": "Yo Adrian"},
+    {"movie": "Terminator", "quote": "I'll be back"},
+    {"movie": "Titanic", "quote": "I'm the king of the world!"},
+    {"movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die."}
+  ];
+
+  onSubmit(): void {
+    console.log("submitted: ", this.formMovieQuote);
+    this.movieQuotes.unshift(this.formMovieQuote);
+    this.formMovieQuote = {
+      'quote': '',
+      'movie': ''
+    };
+
+  }
 }
